ff8a73c255fa447ed574d9c658ca71e33c32f53e
David Jiang <djiang86@binghamton.edu>
2018-01-06 03:32:04 +0000

    using @Qualifier to select the specific class to inject

---
+++ Java Spring/spring-annotations/src/com/spring/springdemo/RestFortuneService.java
@@ -0,0 +1,13 @@
+package com.spring.springdemo;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class RestFortuneService implements FortuneService {
+
+	@Override
+	public String getFortune() {
+		return null;
+	}
+
+}
--- Java Spring/spring-annotations/src/com/spring/springdemo/TennisCoach.java
+++ Java Spring/spring-annotations/src/com/spring/springdemo/TennisCoach.java
@@ -1,12 +1,14 @@
 package com.spring.springdemo;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Component;
 
 @Component
 public class TennisCoach implements Coach {
 
 	@Autowired
+	@Qualifier("happyFortuneService")
 	private FortuneService fortuneService;
 	
 	// define a default constructor
