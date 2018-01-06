55bb2b1b7fad44d87e4cb6d7b5df73d22e5c3169
David Jiang <djiang86@binghamton.edu>
2018-01-06 03:00:57 +0000

    method/field injections using @autowired

--- Java Spring/spring-annotations/src/com/spring/springdemo/TennisCoach.java
+++ Java Spring/spring-annotations/src/com/spring/springdemo/TennisCoach.java
@@ -6,6 +6,7 @@ import org.springframework.stereotype.Component;
 @Component
 public class TennisCoach implements Coach {
 
+	@Autowired
 	private FortuneService fortuneService;
 	
 	// define a default constructor
@@ -15,17 +16,22 @@ public class TennisCoach implements Coach {
 	
 	
 	//define a setter method
-	@Autowired
-	public void setFortuneService(FortuneService fortuneService) {
-		System.out.println(">> TennisCoach: inside setFortuenService() method");
-		this.fortuneService = fortuneService;
-	}
-	
 //	@Autowired
-//	public TennisCoach(FortuneService fortuneService) {
+//	public void setFortuneService(FortuneService fortuneService) {
+//		System.out.println(">> TennisCoach: inside setFortuenService() method");
 //		this.fortuneService = fortuneService;
 //	}
-
+//	
+////	@Autowired
+////	public TennisCoach(FortuneService fortuneService) {
+////		this.fortuneService = fortuneService;
+////	}
+//
+//	@Autowired
+//	public void doSomeCrazyStuff(FortuneService fortuneService) {
+//		System.out.println(">> TennisCoach: inside doSomeCrazyStuff() method");
+//		this.fortuneService = fortuneService;
+//	}
 	
 	@Override
 	public String getDailyWorkout() {
