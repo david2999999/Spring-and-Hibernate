4d2c8af02bd9e0d26fa2e0c45f977b30ec10607f
David Jiang <djiang86@binghamton.edu>
2018-01-08 22:00:22 +0000

    trimming string input from forms. This prevents user from entering empty strings

--- Java Spring/Spring-MVC-Demo/src/com/spring/springdemo/mvc/CustomerController.java
+++ Java Spring/Spring-MVC-Demo/src/com/spring/springdemo/mvc/CustomerController.java
@@ -2,15 +2,28 @@ package com.spring.springdemo.mvc;
 
 import javax.validation.Valid;
 
+import org.springframework.beans.propertyeditors.StringTrimmerEditor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.WebDataBinder;
+import org.springframework.web.bind.annotation.InitBinder;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 @RequestMapping("/customer")
 public class CustomerController {
+	
+	// add an initbinder to convert trim input strings
+	// remove leading and trailing whitespace
+	// resolve issues for our validation where the user can't enter white spaces
+	@InitBinder
+	public void initBinder(WebDataBinder dataBinder) {
+		StringTrimmerEditor stringTrimmerEditor = new StringTrimmerEditor(true);
+		dataBinder.registerCustomEditor(String.class, stringTrimmerEditor);
+	}
+	
 	
 	@RequestMapping("/showForm")
 	public String showForm(Model theModel) {
