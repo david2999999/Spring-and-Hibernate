78ff7b5792eb66787245743fa313a16c44a5048e
David Jiang <djiang86@binghamton.edu>
2018-01-08 01:06:42 +0000

    retrieving data from form by using (HttpServletRequest request, Model model)

--- Java Spring/Spring-MVC-Demo/WebContent/WEB-INF/view/main-menu.jsp
+++ Java Spring/Spring-MVC-Demo/WebContent/WEB-INF/view/main-menu.jsp
@@ -2,9 +2,11 @@
 <html>
 <head>
 	<meta http-equiv="Content-Type" content="text/html; charset=ISO-8859-1">
-	<title>Insert title here</title>
+	<title>MVC DEMOOOO</title>
 </head>
 <body>
 	<h2>Spring MVC Demo - Home Page</h2>
+	<hr>
+	<a href="showForm">Hello World form</a>
 </body>
 </html>
---
+++ Java Spring/Spring-MVC-Demo/src/com/spring/springdemo/mvc/HelloWorldController.java
@@ -0,0 +1,59 @@
+package com.spring.springdemo.mvc;
+
+import javax.servlet.http.HttpServletRequest;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import com.sun.net.httpserver.HttpServer;
+
+@Controller
+public class HelloWorldController {
+
+	// need a controller method to show the initial HTML form
+	@RequestMapping("/showForm")
+	public String showForm() {
+		return "helloworld-form";
+	}
+	
+	
+	// need a controller method to process the HTML form
+	@RequestMapping("/processForm")
+	public String processForm() {
+		return "helloworld";
+	}
+	
+	// another method of retrieving data from a form
+	@RequestMapping("/processFormV2")
+	public String shoutTheName(HttpServletRequest request, Model model) {
+		
+		// read the request parameter from the HTML form
+		String theName = request.getParameter("studentName");
+		
+		//convert the data to all caps
+		theName = theName.toUpperCase();
+		
+		//create the message
+		String result = "HELLO! " + theName;
+		
+		// add the message to the model
+		model.addAttribute("message", result);
+		
+		return "helloworld";
+	}
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+}
