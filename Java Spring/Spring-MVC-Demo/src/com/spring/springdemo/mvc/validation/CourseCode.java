1f19217b79a1381f1b7308c58a0ad9ed9c8fb516
David Jiang <djiang86@binghamton.edu>
2018-01-09 02:09:39 +0000

    creating custom java annotations

---
+++ Java Spring/Spring-MVC-Demo/src/com/spring/springdemo/mvc/validation/CourseCode.java
@@ -0,0 +1,31 @@
+package com.spring.springdemo.mvc.validation;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+import javax.validation.Constraint;
+
+@Constraint(validatedBy = CourseCodeConstraintValidator.class)
+@Target({ElementType.METHOD, ElementType.FIELD})
+@Retention(RetentionPolicy.RUNTIME)
+public @interface CourseCode {
+
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+}
