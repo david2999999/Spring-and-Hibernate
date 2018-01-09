f082810cf1020649b2414da6096acfff294bf030
David Jiang <djiang86@binghamton.edu>
2018-01-09 00:02:14 +0000

    custom error warnings using property files

--- Java Spring/Spring-MVC-Demo/src/com/spring/springdemo/mvc/Customer.java
+++ Java Spring/Spring-MVC-Demo/src/com/spring/springdemo/mvc/Customer.java
@@ -15,9 +15,10 @@ public class Customer {
 	@Size(min=1, message="is required")
 	private String lastName;
 
+	@NotNull(message="is required")
 	@Min(value=0, message="Value must be greater or equal to zero")
 	@Max(value=10, message="Value must be less than or equal to ten")
-	private int freePasses;
+	private Integer freePasses;
 	
 	@Pattern(regexp="^[a-zA-Z0-9]{5}", message="only 5 chars/digits")
 	private String postalCode; 
@@ -46,11 +47,11 @@ public class Customer {
 		this.lastName = lastName;
 	}
 
-	public int getFreePasses() {
+	public Integer getFreePasses() {
 		return freePasses;
 	}
 
-	public void setFreePasses(int freePasses) {
+	public void setFreePasses(Integer freePasses) {
 		this.freePasses = freePasses;
 	}
 
