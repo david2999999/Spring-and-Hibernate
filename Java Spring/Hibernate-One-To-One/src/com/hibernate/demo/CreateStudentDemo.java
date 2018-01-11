6c2706f846009f61f58c214dd933ef66b4255583
David Jiang <djiang86@binghamton.edu>
2018-01-11 17:41:32 +0000

    started hibernate one to one

---
+++ Java Spring/Hibernate-One-To-One/src/com/hibernate/demo/CreateStudentDemo.java
@@ -0,0 +1,57 @@
+package com.hibernate.demo;
+
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.cfg.Configuration;
+
+import com.hibernate.demo.entity.Student;
+
+public class CreateStudentDemo {
+
+	public static void main(String[] args) {
+		
+		// create a session factory
+		SessionFactory factory = new Configuration()
+						.configure("hibernate.cfg.xml")
+						.addAnnotatedClass(Student.class)
+						.buildSessionFactory();
+		
+		
+		// create a session
+		Session session = factory.getCurrentSession();
+		
+		try {
+			// create a student object
+			System.out.println("Creating new student object...");
+			Student student = new Student("David", "Duck", "hello123@gmail.com");
+			
+			// start a transaction
+			session.beginTransaction();
+			
+			//save the student object
+			System.out.println("Saving the student object");
+			session.save(student);
+			
+			//commit transaction
+			session.getTransaction().commit();
+			
+			System.out.println("Done!");
+		}finally {
+			factory.close();
+		}
+	}
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
