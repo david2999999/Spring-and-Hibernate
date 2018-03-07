465027591e0e872bcc45b73583ef7e5849ded18e
David Jiang <djiang86@binghamton.edu>
2018-03-07 23:17:44 +0000

    associating instructor with 1 to many courses

---
+++ Java Spring/Hibernate-03-One-To-Many/src/com/hibernate/demo/GetInstructorCoursesDemo.java
@@ -0,0 +1,63 @@
+package com.hibernate.demo;
+
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.cfg.Configuration;
+
+import com.hibernate.demo.entity.Course;
+import com.hibernate.demo.entity.Instructor;
+import com.hibernate.demo.entity.InstructorDetail;
+
+public class GetInstructorCoursesDemo {
+
+	public static void main(String[] args) {
+		
+		// create a session factory
+		SessionFactory factory = new Configuration()
+						.configure("hibernate.cfg.xml")
+						.addAnnotatedClass(Instructor.class)
+						.addAnnotatedClass(InstructorDetail.class)
+						.addAnnotatedClass(Course.class)
+						.buildSessionFactory();
+		
+		
+		// create a session
+		Session session = factory.getCurrentSession();
+		
+		try {
+			// start a transaction
+			session.beginTransaction();
+			
+			// get the instructor from db
+			int theId = 1;
+			Instructor tempInstructor = session.get(Instructor.class, theId);
+			
+			System.out.println("Instructor: " + tempInstructor);
+			
+			// get course for the instructor 
+			System.out.println("Courses: " + tempInstructor.getCourses().get(0).getTitle());
+			
+			//commit transaction
+			session.getTransaction().commit();
+			
+			System.out.println("Done!");
+		}finally {
+			session.close();
+			factory.close();
+		}
+	}
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
