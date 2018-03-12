0623190b44ca8d0f61a3ce96609e74c1c4afea8b
David Jiang <djiang86@binghamton.edu>
2018-03-12 03:10:49 +0000

    using @afterThrowing advice notation

---
+++ Spring and Hibernate/spring-demo-aop-after-throwing/src/com/spring/aopdemo/AfterThrowingDemoApp.java
@@ -0,0 +1,41 @@
+package com.spring.aopdemo;
+
+import java.util.List;
+
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
+import com.spring.aopdemo.dao.AccountDAO;
+import com.spring.aopdemo.dao.MembershipDAO;
+
+public class AfterThrowingDemoApp {
+
+	public static void main(String[] args) {
+		// read spring config java class
+		AnnotationConfigApplicationContext context =
+				new AnnotationConfigApplicationContext(DemoConfig.class);
+		
+		// get the bean from the spring container
+		AccountDAO theAccountDAO = context.getBean("accountDAO", AccountDAO.class);
+		
+		List<Account> theAccounts = null;
+		
+		try {
+			// add  a boolean flag to simulate exception
+			boolean tripWire = true;
+			theAccounts = theAccountDAO.findAccounts(tripWire);
+			
+		} catch (Exception e) {
+			System.out.println("\nMain program: CALLING EXCEPTION" + e);
+		}
+
+		
+		// display the accounts
+		System.out.println("\nMain program: AFTERTHROWING DEMO APP");
+		System.out.println(theAccounts);
+		
+		// close the context
+		context.close();
+		
+	}
+
+}
